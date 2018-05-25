c6b21e9cd9db5b88e9cd8e5e21107c6b866ebc1b
aa11ac <ach.aayush.111@gmail.com>
2018-05-25 11:09:08 +0545

    changed functionality

--- function/js.js
+++ function/js.js
@@ -6,21 +6,34 @@ $(document).ready(function(){
    var s = parseInt(document.querySelector("#seconds").textContent);
    var clockClass=document.querySelector(".clock"); 
     var clo = document.querySelector(".clo");  
+    var reset=document.querySelector(".reset");
+
     $("#addDay").click(function () {
+        if (d<365){
         d += 1;
-        $("#days").text(d);
+        $("#days").text(d);}
+        else{
+            d=365
+        }
     })
     $("#addHrs").click(function () {
+        if(h<24){
         h += 1;
-        $("#hours").text(h);
+        $("#hours").text(h);}
+        else{
+            h=24;
+        }
     })
    $("#addSec").click(function () {
-       s +=1;
-       $("#seconds").text(s);
+       if(s<59){s +=1;
+       $("#seconds").text(s);}
+       else{
+           s=59
+       }
    })
    $("#addMin").click(function () {
-       m += 1;
-       $("#minutes").text(m);
+      if(m<59) {m += 1;
+       $("#minutes").text(m);}
    })
    
    $(".subtractSecond").click(function () {
@@ -60,16 +73,47 @@ $(document).ready(function(){
        $(".caption").fadeOut();
        $("body").css("background-color", "black");
        $("body").css("color", "white");
+       
         clockClass.classList.toggle("clicked");
        clo.classList.toggle("clicked");
-       var m= parseInt(document.querySelector("#minutes").textContent);
-       var s = parseInt(document.querySelector("#seconds").textContent);
-       var h = parseInt(document.querySelector("#hours").textContent);
-       var d = parseInt(document.querySelector("#days").textContent);
-      function countDown(){
-          
-      }
-        
+       var minutes= parseInt(document.querySelector("#minutes").textContent);
+       var seconds = parseInt(document.querySelector("#seconds").textContent);
+       var hours = parseInt(document.querySelector("#hours").textContent);
+       var days = parseInt(document.querySelector("#days").textContent);
+       function calculate() {
+           setTimeout(calculate, 1000);
+           $('#days').html(days);
+           $("#hours").html(hours);
+           $("#minutes").html(minutes);
+           $("#seconds").html(seconds);
+           seconds--;
+           if (seconds < 0) {
+               seconds = 59;
+               minutes--;
+               if (minutes < 0) {
+                   hours--;
+                   minutes = 59;
+                   if (hours < 0) {
+                       days--;
+                       hours = 23;
+                       if (days < 0) {
+                           days = 0;
+                           hours = 0;
+                           minutes = 0;
+                           seconds = 0;
+                           $(".info").remove();
+                           $(".clock").html("COMPLETED");
+                           var audio=document.querySelector("audio");
+                           audio.play();
+                           document.querySelector(".reset").classList.remove("hidden");
+
+                   }
+               }
+           }
+       }
+    }
+       calculate();
+
    })
 
 
